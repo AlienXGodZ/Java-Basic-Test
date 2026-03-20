0d1a13a23b34bfd135763deb04b5c3acd1138ce8
AlienXGodZ <allenharvey2005@gmail.com>
2026-03-20 15:16:23 +0530

    test2 added

---
+++ Test2.java
@@ -0,0 +1,27 @@
+
+// Palindrome program using Strings
+
+public class Test2 {
+    public static void main(String[] args) {
+        String letter = "allena";
+        int length = letter.length() -1;
+        int start = 0;
+        boolean condition = true;
+
+        while(start < length){
+            if (letter.charAt(start) != letter.charAt(length)){
+                condition = false;
+                break;
+            }
+            start++;
+            length--;
+
+        }
+        if(condition) {
+            System.out.println("Palindrome");
+        }
+        else{
+            System.out.println("Not Palindrome");
+        }
+    }
+}
