559a6244b00b956c7034d132db7878a856387acd
AlienXGodZ <allenharvey2005@gmail.com>
2026-03-20 22:22:15 +0530

    Test13 added

---
+++ Test13.java
@@ -0,0 +1,12 @@
+// this is my first mistake. but this reverse the array but not this reverse only number not array.
+
+public class Test13{
+    public static void main (String[] args){
+        int[] arr = {10,20,30,40};
+
+
+            for(int i = arr.length -1; i >= 0; i--){
+                System.out.print(arr[i] + " ");
+            }
+        }
+    }
