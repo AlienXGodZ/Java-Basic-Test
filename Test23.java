407d84b2b9abe8168c54bb01e4ff879c24cac14d
AlienXGodZ <allenharvey2005@gmail.com>
2026-03-21 01:04:35 +0530

    Test23 added

---
+++ Test23.java
@@ -0,0 +1,17 @@
+// swapping the String
+
+public class Test23{
+    public static void main(String[] args){
+        String a = "Allen";
+        String b = "Joan";
+
+        String temp = a;
+        a = b;
+        b = temp;
+
+
+
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
