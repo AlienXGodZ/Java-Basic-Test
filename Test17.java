89f3c5bd6905950a4686759c1e9bca62bc3714bf
AlienXGodZ <allenharvey2005@gmail.com>
2026-03-20 23:29:17 +0530

    Test17 java

---
+++ Test17.java
@@ -0,0 +1,13 @@
+// checking which number is odd and which number is even
+
+public class Test17{
+    public static void main(String[] args){
+        int number = 3;
+
+        if(number % 2 == 0 ) {
+            System.out.println(number + " is EvenNumber");
+        }else{
+            System.out.println(number + " is OddNumber");
+        }
+    }
+        }
