7eff322307d33969142d127758562a798cdbcc6e
AlienXGodZ <allenharvey2005@gmail.com>
2026-03-20 16:06:22 +0530

    Test4 added

---
+++ Test4.java
@@ -0,0 +1,11 @@
+// count the single word program
+
+public class Test4{
+    public static void main (String[] args){
+        String letter = "AllenHarvey";
+
+        int storelength = letter.length();
+
+        System.out.println(storelength);
+    }
+}
