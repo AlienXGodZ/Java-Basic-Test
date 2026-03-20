dcd9a964fd013846ac10c4e096e08d6e78107e9d
AlienXGodZ <allenharvey2005@gmail.com>
2026-03-20 14:37:41 +0530

    test 1 java program added

--- Test1.java
+++ Test1.java
@@ -1,1 +1,16 @@
+//revers a Number Program
 
+public class Test1{
+    public static void main(String[] args){
+
+        int number = 1234;
+        int store = 0;
+
+        while(mumber > 0){
+            int takelastdigit = number % 10;   //take last digit
+            store = store * 10 + lastdigit;   //store digit
+            number = number / 10;            //Remove last digit
+        }
+
+        System.out.println(store);
+    }
