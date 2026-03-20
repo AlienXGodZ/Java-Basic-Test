c916b3ad07fe44801312ea76e10656652948da35
AlienXGodZ <allenharvey2005@gmail.com>
2026-03-21 00:52:25 +0530

    Test21 added

---
+++ Test21.java
@@ -0,0 +1,18 @@
+// swapping the all numbers
+
+public class Test21{
+    public static void main(String[] args){
+        int a = 10;
+        int b = 21;
+        int c = 22;
+
+        int temp = a;
+         a = b;
+         b = c;
+         c = temp;
+
+         System.out.println(a);
+         System.out.println(b);
+         System.out.println(c);
+    }
+}
