c8c38b9affe6e2ec6980e5af2063a742eb9c5269
AlienXGodZ <allenharvey2005@gmail.com>
2026-03-20 21:31:01 +0530

    Test11 is added

---
+++ Test11.java
@@ -0,0 +1,17 @@
+//finding which number is greatest
+
+public class Test11{
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 2;
+        int c = 15;
+
+        if (a > b && a > c) {
+            System.out.println("a is greatest");
+        } else if (b > a && b > c){
+        System.out.println("b is greatest");
+           }else if( c > a && c > b){
+            System.out.println("c is greatest");
+            }
+        }
+    }
