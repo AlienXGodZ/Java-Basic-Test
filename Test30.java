d948d951bffd0f0c210a96ec7a30464fd7145b6a
AlienXGodZ <allenharvey2005@gmail.com>
2026-03-21 15:22:05 +0530

    Test30 added

---
+++ Test30.java
@@ -0,0 +1,14 @@
+// count numbers using log10 easy tricky and magic
+
+public class Test30{
+    public static void main(String[] args){
+        // count the single numbers
+
+                int number = 45764832;
+
+                int store = (int)Math.log10(number)+1;
+
+                System.out.println(store);
+            }
+        }
+
