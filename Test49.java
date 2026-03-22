0aad2ef5c4415736a87c8297c86a0ba8678a61b3
AlienXGodZ <allenharvey2005@gmail.com>
2026-03-22 21:51:23 +0530

    Test 49 added

---
+++ Test49.java
@@ -0,0 +1,15 @@
+// swap without temp
+
+public class Test49{
+    public static void main(String[] args){
+        int a = 100;
+        int b = 2;
+
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        System.out.println(" a = " + a);
+        System.out.println(" b = " + b);
+    }
+}
