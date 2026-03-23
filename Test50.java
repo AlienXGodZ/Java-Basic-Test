9b135bb17d598b50e05628756142e9d914008ad8
AlienXGodZ <allenharvey2005@gmail.com>
2026-03-23 12:42:04 +0530

    Test50 added

---
+++ Test50.java
@@ -0,0 +1,17 @@
+//Fibonacci Series (n terms)
+
+public class Test50{
+    public static void main(String[] args){
+        int n = 5;
+
+        int a = 0;
+        int b = 1;
+         System.out.print(a + " " + b + " ");
+        for(int i = 3 ; i <= n ; i++){
+            int c = a + b;
+            System.out.print(c + " ");
+            a = b;
+            b = c;
+        }
+    }
+}
