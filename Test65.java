00f24abfe59866e93510b5893a736f7ed639c915
AlienXGodZ <allenharvey2005@gmail.com>
2026-03-24 13:17:00 +0530

    Test65 added

---
+++ Test65.java
@@ -0,0 +1,15 @@
+//Sum of Even Numbers Only
+
+public class Test65{
+    public static void main(String[] args){
+        int[] numbers = {1,2,3,4,5,6,7,8,9,10};
+        int store = 0;
+
+        for(int i = 0; i < numbers.length ; i++){
+            if(numbers[i] % 2 == 0){
+                store = store + numbers[i];
+            }
+        }
+        System.out.println(store);
+    }
+}
