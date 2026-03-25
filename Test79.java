e8f10f938c9870b189cd195993416f16b7c6b748
AlienXGodZ <allenharvey2005@gmail.com>
2026-03-25 15:55:46 +0530

    Test80 added

---
+++ Test79.java
@@ -0,0 +1,19 @@
+//Rotate Array Right by 1
+
+public class Test79 {
+    public static void main(String[] args) {
+
+        int[] arr = {1, 2, 3, 4};
+
+        int last = arr[arr.length - 1];
+
+        for (int i = arr.length - 1; i > 0; i--) {
+            arr[i] = arr[i - 1];
+        }
+        arr[0] = last;
+
+        for(int i = 0 ; i < arr.length ; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
