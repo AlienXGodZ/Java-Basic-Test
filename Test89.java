b9330e575d0b6fa9e874a71f695a21be3fa3a851
AlienXGodZ <allenharvey2005@gmail.com>
2026-03-25 19:51:20 +0530

    Test89 added

---
+++ Test89.java
@@ -0,0 +1,10 @@
+// converting into decimal into binary using built in methods.
+
+public class Test89{
+    public static void main(String[] args){
+        int n = 10;
+
+        String binary = Integer.toBinaryString(n);
+        System.out.println(binary);
+     }
+}
