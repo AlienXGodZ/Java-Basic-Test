5eec1680369b95881438f9f29bb933d861f48607
AlienXGodZ <allenharvey2005@gmail.com>
2026-03-28 14:52:48 +0530

    Test130 added

---
+++ Test130.java
@@ -0,0 +1,13 @@
+//Using math power
+
+public class Test130 {
+    public static void main(String[] args) {
+
+        int a = 2;
+        int b = 3;
+
+        double result = Math.pow(a, b);
+
+        System.out.println("Power: " + (int)result);
+    }
+}
