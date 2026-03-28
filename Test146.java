d356063a6827a010c318c9ccc90120e30e7cb3fe
AlienXGodZ <allenharvey2005@gmail.com>
2026-03-29 00:47:40 +0530

    Test146 added

---
+++ Test146.java
@@ -0,0 +1,20 @@
+//Star Pyramid (Odd stars)
+
+public class Test146 {
+    public static void main(String[] args) {
+
+        int n = 4;
+
+        for (int i = 1; i <= n; i++) {
+
+            int num = i % 2; // start with 1 or 0
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print(num);
+                num = 1 - num; // toggle 0 ↔ 1
+            }
+
+            System.out.println();
+        }
+    }
+}
