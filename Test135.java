b4af2093ef5e616b4cb40604237d3a4bfed5add2
AlienXGodZ <allenharvey2005@gmail.com>
2026-03-28 15:37:54 +0530

    Test135 added

---
+++ Test135.java
@@ -0,0 +1,17 @@
+//find common element in the 2 array
+
+public class Test135 {
+    public static void main(String[] args) {
+
+        int[] a = {1,2,3,4};
+        int[] b = {3,4,5,6};
+
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < b.length; j++) {
+                if (a[i] == b[j]) {
+                    System.out.print(a[i] + " ");
+                }
+            }
+        }
+    }
+}
