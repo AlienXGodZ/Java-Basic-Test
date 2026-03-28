94a0d1193935af20177c81fbe07568d610657417
AlienXGodZ <allenharvey2005@gmail.com>
2026-03-28 16:13:36 +0530

    Test138 added

---
+++ Test138.java
@@ -0,0 +1,17 @@
+//Print all substrings
+public class Test138 {
+    public static void main(String[] args) {
+
+        String str = "abc";
+
+        for (int i = 0; i < str.length(); i++) {
+            for (int j = i; j < str.length(); j++) {
+
+                for (int k = i; k <= j; k++) {
+                    System.out.print(str.charAt(k));
+                }
+                System.out.println();
+            }
+        }
+    }
+}
