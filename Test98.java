f40451e38c6a22a601d565c7fa14c9bbafb05856
AlienXGodZ <allenharvey2005@gmail.com>
2026-03-29 00:30:10 +0530

    Test144 added

--- Test98.java
+++ Test98.java
@@ -2,7 +2,7 @@
 
 public class Test98{
     public static void main(String[] args){
-        int n = 5;
+        int n = 3;
         for(int i = 1; i <= n; i++){
             for(int j = 1; j <= n-i; j++) System.out.print(" "); // spaces
             for(int j = 1; j <= 2*i-1; j++) System.out.print("*"); // stars
