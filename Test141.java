8ffd46547fd601232c82d1725b7abd01710703b0
AlienXGodZ <allenharvey2005@gmail.com>
2026-03-28 23:21:03 +0530

    Test141 added

---
+++ Test141.java
@@ -0,0 +1,13 @@
+//Sum of digits (recursion)
+public class Test141 {
+    public static int sum(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        return (n % 10) + sum(n / 10);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sum(123));
+    }
+}
