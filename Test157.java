323b1454b153d613eb94dcfa3a3e071da83cb90d
AlienXGodZ <allenharvey2005@gmail.com>
2026-03-29 15:15:41 +0530

    Test157 added

---
+++ Test157.java
@@ -0,0 +1,13 @@
+//finding Area of triangle
+
+public class Test157 {
+    public static void main(String[] args) {
+
+        int base = 10;
+        int height = 6;
+
+        int area = (base * height) / 2;
+
+        System.out.println("Area of Triangle = " + area);
+    }
+}
