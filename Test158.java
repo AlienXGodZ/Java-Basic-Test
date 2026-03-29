24b7949ee836d479fc4e248d3314dc512cfd6c97
AlienXGodZ <allenharvey2005@gmail.com>
2026-03-29 15:17:58 +0530

    Test158 added

---
+++ Test158.java
@@ -0,0 +1,14 @@
+//finding square and cube
+
+public class Test158 {
+    public static void main(String[] args) {
+
+        int n = 4;
+
+        int square = n * n;
+        int cube = n * n * n;
+
+        System.out.println("Square = " + square);
+        System.out.println("Cube = " + cube);
+    }
+}
