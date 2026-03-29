2868afd61a34c121ac1ee739cac1263fc970edfe
AlienXGodZ <allenharvey2005@gmail.com>
2026-03-29 14:18:00 +0530

    Test151 added

---
+++ Test151.java
@@ -0,0 +1,13 @@
+//convert celsius into fahrenheit
+
+public class Test151 {
+    public static void main(String[] args) {
+
+        double celsius = 25;
+        double fahrenheit;
+
+        fahrenheit = (9.0/5) * celsius + 32;
+
+        System.out.println("Fahrenheit: " + fahrenheit);
+    }
+}
---
+++ Test152.java
@@ -0,0 +1,13 @@
+//convert fahrenheit into celsius
+
+public class Test152 {
+    public static void main(String[] args) {
+
+        double fahrenheit = 25;
+        double celsius;
+
+        celsius = (fahrenheit - 32) * 5.0 / 9;
+
+        System.out.println("celsius: " + celsius);
+    }
+}
