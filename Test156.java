28a59850ee3995c660d79f7e9f0be945fba54575
AlienXGodZ <allenharvey2005@gmail.com>
2026-03-29 15:12:38 +0530

    Test156 added

---
+++ Test156.java
@@ -0,0 +1,11 @@
+//finding area of circle
+
+public class Test156 {
+    public static void main(String[] args) {
+
+        double r = 5;
+        double area = 3.14 * r * r;   // use can use Math.pi
+
+        System.out.println("Area of Circle = " + area);
+    }
+}
