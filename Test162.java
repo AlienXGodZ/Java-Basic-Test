393ecd6375a097b98c31c3c60f6cd25654a3e7a0
AlienXGodZ <allenharvey2005@gmail.com>
2026-06-01 13:47:56 +0530

    Test162 added

---
+++ Test162.java
@@ -0,0 +1,21 @@
+public class Test162 {
+    public static void main(String[] args) {
+
+        myMethod(5);
+
+
+    }
+
+    public static void myMethod(int n){
+
+        if(n == 0){
+            return ;
+        }
+
+        myMethod(n-1);
+        System.out.println("HELLO");
+
+
+    }
+
+}
