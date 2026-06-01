d494b3859ff7a89c88a155e30c7de4d46b924b59
AlienXGodZ <allenharvey2005@gmail.com>
2026-06-01 14:29:29 +0530

    Test168 added

--- Test168.java
+++ Test168.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
-
 public class Test168 {
+    public static void main(String[] args) {
+
+        System.out.println(myMethod(12345));
+
+
+    }
+
+
+    public static int myMethod(int n){
+        if(n == 0){
+            return 0;
+        }
+
+
+         return 1 + myMethod(n/10);
+
+
+
+
+
+
+
+
+    }
+
 }
