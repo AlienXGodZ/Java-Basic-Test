bb3d1488998b8c47e034489b9d263765238f3361
AlienXGodZ <allenharvey2005@gmail.com>
2026-06-01 23:28:07 +0530

    Test175 added

---
+++ Test175.java
@@ -0,0 +1,19 @@
+public class Test175 {
+    public static void main(String[] args) {
+
+        myMethod(10);
+
+
+
+    }
+
+    public static void myMethod(int n){
+
+        if(n == 0) return ;
+
+        myMethod(n-1);
+
+        if(n % 2 != 0) System.out.print(n + " ");
+
+    }
+}
